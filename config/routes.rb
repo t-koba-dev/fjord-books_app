2b56704a5654b643663da26fa875449f4a76f468
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-08 13:51:36 +0900

    letter_openerをマウントさせる

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'books#index'
   resources :books
   resources :users, only: %i[index show]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
