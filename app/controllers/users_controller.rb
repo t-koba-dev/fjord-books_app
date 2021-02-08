8aa0e410cf073b30a8f46f8c1ce18510044f9987
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-08 14:16:24 +0900

    ログインしていないと本一覧を見れないよう設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+  before_action :check_if_user_signed_in
+
   def index
     @users = User.order(id: :desc).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def check_if_user_signed_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
