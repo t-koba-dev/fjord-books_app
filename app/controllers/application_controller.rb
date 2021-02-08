aff3f17fd2f603e137f8e2e3294e17995d6f0882
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-08 11:06:53 +0900

    郵便番号、住所、自己紹介を編集できるよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :postal_code, :address, :self_introduction])
   end
 end
