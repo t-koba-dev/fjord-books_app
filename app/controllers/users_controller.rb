4c00a71c1b51db61943340840c9bbe9b7e002f85
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-08 13:22:11 +0900

    usersコントローラーのshowアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.order(id: :desc).page(params[:page])
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
