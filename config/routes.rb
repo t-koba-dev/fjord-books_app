7dcfb285f842fb577376bcf1bc0b86fc45051648
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-08 16:48:42 +0900

    不要なコメントアウトを削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   resources :books
   resources :users, only: %i[index show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
