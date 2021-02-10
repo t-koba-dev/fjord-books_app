d010446ecd75679b1c1c73f6dc243090aeb6e212
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-10 19:54:51 +0900

    rubocopによる整形

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,11 @@
-class Users::RegistrationsController < Devise::RegistrationsController
+# frozen_string_literal: true
 
+class Users::RegistrationsController < Devise::RegistrationsController
   protected
+
   def update_resource(resource, params)
-    if params["password"].present? || params["password_confirmation"].present?
-      return super
-    end
-    resource.update_without_password(params.except("current_password"))
+    return super if params['password'].present? || params['password_confirmation'].present?
+
+    resource.update_without_password(params.except('current_password'))
   end
 end
