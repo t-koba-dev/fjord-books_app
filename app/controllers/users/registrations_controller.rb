9c46e6b7a7bc1788bbaa0eac3086fc1c62e1bb49
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-10 19:39:06 +0900

    passwordとpassword_confirmationのどちらかに値が入力されているときは、スーパークラスでcurrent_passwordを検証する

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,10 +6,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
   def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
-
-  def update_without_password(params, *options)
-    super(params)
+    if params["password"].present? || params["password_confirmation"].present?
+      return super
+    end
+    resource.update_without_password(params.except("current_password"))
   end
 end
