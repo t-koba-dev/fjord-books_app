a39e20fad27d0365e75b1555af72b58506f355c0
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-10 19:42:14 +0900

    不要なメソッドを削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def build_resource(hash = {})
-    hash[:uid] = User.create_unique_string
-    super
-  end
 
   protected
   def update_resource(resource, params)
