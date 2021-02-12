d4d10e05922a131b8c8bc4375fb18416132d10f6
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-12 14:48:22 +0900

    ユーザー削除時に、該当ユーザーに関連するRelationshipも削除するようアソシエーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
   # relationship
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
