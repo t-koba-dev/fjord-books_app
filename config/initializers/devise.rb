9fe452cd0464445b4f56214c8b265852cd54e8fe
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-13 13:55:01 +0900

    不要な改行を削除

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Devise.setup do |config|
-
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
