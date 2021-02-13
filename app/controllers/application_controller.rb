22924727cfe0c013a74cd20f648de1fc9ea41658
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-13 20:45:28 +0900

    未サインイン時のログイン画面遷処理をdeviseデフォルトのメソッドに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :check_if_user_signed_in
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def check_if_user_signed_in
-    redirect_to new_user_session_path unless user_signed_in? || (request.path == new_user_session_path)
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
