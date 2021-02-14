1b1817c0e537aa46369181ab5e8fe5aa0dbeeb07
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-14 10:54:14 +0900

    rubocopによる整形

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
