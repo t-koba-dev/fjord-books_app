b05905b250d40aed5650b36e6005b1784aff2838
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-15 12:02:43 +0900

    日報関連の単語を日本語化

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,7 +19,7 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
 
     if @report.save
-      redirect_to @report, notice: 'Report was successfully created.'
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new
     end
@@ -27,7 +27,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      redirect_to @report, notice: 'Report was successfully updated.'
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy
-    redirect_to reports_url, notice: 'Report was successfully destroyed.'
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
