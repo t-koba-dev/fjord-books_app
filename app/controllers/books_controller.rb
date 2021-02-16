28906e669b99962bc315a78ea721166b230c44b7
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-16 15:38:45 +0900

    本のコメントに関するコントローラーを作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,10 @@ class BooksController < ApplicationController
 
   # GET /books/1
   # GET /books/1.json
-  def show; end
+  def show
+    @book = Book.find(params[:id])
+    @comment = @book.comments.new
+  end
 
   # GET /books/new
   def new
