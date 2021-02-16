0b51c6f792ffb6a350792f118b6fae47dad33fc9
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-16 15:32:33 +0900

    booksにネストしたcommentsのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   root to: 'books#index'
-  resources :books
+  resources :books do
+    resources :comments, module: :books
+  end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
