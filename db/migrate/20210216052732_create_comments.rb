644f23e81853ee549090a81f1d9c7bb6cffe1895
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-16 14:29:41 +0900

    commentsテーブルを作成

---
+++ db/migrate/20210216052732_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.references :user, foreign_key: true
+      t.text :body
+      t.references :commentable, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end
