5e6b6cad9ff5f965ef0c82523b7c65f8b3e68706
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-16 15:05:23 +0900

    日報が編集できるかを判断するメソッドをテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?" do
+    alice = User.create!(email: 'alice1@example.com', password: 'password')
+    bob = User.create!(email: 'bob1@example.com', password: 'password')
+    report = Report.create!(user: alice, title: 'first_report', content: 'hello_bob')
+
+    assert report.editable?(alice)
+    assert_not report.editable?(bob)
+  end
 end
