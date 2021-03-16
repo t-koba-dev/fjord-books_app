77df7b08fa73218807eda61e198df49f68f99c1b
ToshiroKoba <t-koba@ruby-dev.jp>
2021-03-16 15:40:58 +0900

    ユーザーモデルテストの重複処理をまとめる

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,34 +3,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "#name_or_email" do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
+  def setup
+    @alice = User.create!(email: 'alice1@example.com', password: 'password')
+    @bob = User.create!(email: 'bob1@example.com', password: 'password')
+  end
 
-    user.name = 'Foo Bar'
-    assert_equal 'Foo Bar', user.name_or_email
+  test "#name_or_email" do
+    assert_equal 'alice1@example.com', @alice.name_or_email
+
+    @alice.name = 'alice'
+    assert_equal 'alice', @alice.name_or_email
   end
 
   test "#follow" do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    assert_not she.followed_by?(me)
-    me.follow(she)
-    assert me.following?(she)
-    assert she.followed_by?(me)
+    assert_not @alice.following?(@bob)
+    assert_not @bob.followed_by?(@alice)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+    assert @bob.followed_by?(@alice)
   end
 
   test "#unfollow" do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    me.follow(she)
-    assert me.following?(she)
-    assert she.followed_by?(me)
-    me.unfollow(she)
-    assert_not me.following?(she)
-    assert_not she.followed_by?(me)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+    assert @bob.followed_by?(@alice)
+    @alice.unfollow(@bob)
+    assert_not @alice.following?(@bob)
+    assert_not @bob.followed_by?(@alice)
   end
 end
